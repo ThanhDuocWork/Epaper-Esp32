3b6d25c9231d2c96430c7536851adb489cd21af3
Thanh Duoc <thanhduoc.work@gmail.com>
2026-03-01 22:42:07 +0700

    fill full white and black

--- components/epd/include/epd_lvgl.h
+++ components/epd/include/epd_lvgl.h
@@ -3,14 +3,15 @@ void epd_lvgl_init(void);
 #pragma once
 #include <stdint.h>
 #include "lvgl.h"
-
+#include <esp_log.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void epd_lvgl_init(void);                 // init lvgl driver(display)
-void epd_lvgl_flush_full(lv_disp_t *disp); // manual flush full screen
-
+void epd_lvgl_flush_full(void); // manual flush full screen
+void epd_lvgl_fill_screen(int black); // black=1 => full black, black=0 => full white
+void epd_lvgl_poll_refresh(void); 
 #ifdef __cplusplus
 }
 #endif
